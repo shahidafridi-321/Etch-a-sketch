192b10290a944ae6baa47739bd6f0ec87d1ff7e8
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-08-06 12:07:49 +0500

    used prettier to made the code more readable

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', newGrid);
 
 createGrid(nbrOfSquare); // squares creating function is called here
-setInterval(rndcolor, 500); // called function which changes bg of container every 0.5 seconds
+setInterval(rndcolor, 4000); // called function which changes bg of container every 4 seconds
 
 // function for creating grids/squares
 function createGrid(number) {
@@ -14,17 +14,17 @@ function createGrid(number) {
     const square = document.createElement('div');
     container.appendChild(square);
     square.setAttribute('class', 'square');
-    square.style.width = `${((400 - (2 * number)) / number)}px`;
-    square.style.height = `${((400 - (2 * number)) / number)}px`;
+    square.style.width = `${(400 - 2 * number) / number}px`;
+    square.style.height = `${(400 - 2 * number) / number}px`;
   }
   setSqrBg(); // sets the background color of squares
 }
 
-//function for settinf background color
+//function for setting background color
 function setSqrBg() {
   let squares = document.querySelectorAll('.square');
   squares = Array.from(squares);
-  squares.forEach(square => {
+  squares.forEach((square) => {
     square.addEventListener('mouseenter', randomColor);
   });
 }
@@ -46,8 +46,7 @@ function newGrid() {
     createGrid(newNumber);
     setSqrBg();
     btn.addEventListener('click', newGrid); // it is for resetting the container and adding new grid agian and again
-  }
-  else {
+  } else {
     alert('Number should be between 1 and 100');
     newGrid();
   }
@@ -57,4 +56,3 @@ function newGrid() {
 function rndcolor() {
   container.style.backgroundColor = `rgb(${randomNumber(255)} ${randomNumber(255)} ${randomNumber(255)})`;
 }
-
